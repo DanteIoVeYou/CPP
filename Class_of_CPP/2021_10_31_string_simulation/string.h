53e6a06e82357db92f74761ae6c31370e6f6f360
danteking <danteking@aliyun.com>
2021-11-13 21:37:05 +0800

    string simulation

---
+++ Class_of_CPP/2021_10_31_string_simulation/string.h
@@ -0,0 +1,12 @@
+#pragma once
+class string {
+public:
+	string(char* str) {
+
+	}
+
+private:
+	_str;
+	_size;
+	_capacity;
+};
---
+++ Class_of_CPP/2021_10_31_string_simulation/test.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "string.h"
+void Test1(){
+
+}
+int main(){
+	Test1();
+	return 0;
+}
