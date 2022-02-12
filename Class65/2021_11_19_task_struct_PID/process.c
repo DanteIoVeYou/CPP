db1535c034e1c581ef5baee17c4ac95a00d8672b
danteking <danteking@aliyun.com>
2022-02-12 20:43:24 +0800

    task_process// pid//

--- Class65/2021_11_19_task_struct_PID/process.c
+++ Class65/2021_11_19_task_struct_PID/process.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 int main() {
 	while(1){
-			printf("hello process\n");
+			printf("I am a process, my pid is : %d, my father is %d\n",getpid(), getppid());
 			sleep(1);
 	}
 	return 0;
