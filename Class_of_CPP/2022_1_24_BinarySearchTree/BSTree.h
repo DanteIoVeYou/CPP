81c51de87335bb9ee683dcfaedc240423c39a400
danteking <danteking@aliyun.com>
2022-03-06 01:53:13 +0800

    Binary Research Tree

--- Class_of_CPP/2022_1_24_BinarySearchTree/BSTree.h
+++ Class_of_CPP/2022_1_24_BinarySearchTree/BSTree.h
@@ -1,61 +1,407 @@
 #pragma once
- 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+//template <class K>
+//struct BSTreeNode {
+//
+//	K _key;
+//	BSTreeNode<K>* _left;
+//	BSTreeNode<K>* _right;
+//	BSTreeNode(const K& key = 0) :
+//		_key = key,
+//		_left = nullptr,
+//		_right = nullptr{}
+//};
+//
+//
+//
+//template <class K>
+//class BSTree {
+//	typedef BSTreeNode Node;
+//	BSTree() :
+//		_node(nullptr){}
+//
+//	~BSTree() {
+//		_node = nullptr;
+//	}
+//
+//	bool Insert(const K& key) {
+//		if (_root == nullptr) {
+//			_root = new Node(key);
+//			return true;
+//		}
+//		Node* cur = _root;
+//		Node* parent = cur;
+//		while (cur) {
+//			if (cur->_key > key) {
+//				parent = cur;
+//				cur = cur->_left;
+//			}
+//
+//			else if (cur->_key < key) {
+//				parent = cur;
+//				cur = cur->_right;
+//			}
+//			else {
+//				return false;
+//			}
+//		}
+//		
+//		if (parent->_left == cur) {
+//			parent->_left = new Node(key);
+//		}
+//		else {
+//			parent->_right = new Node(key);
+//		}
+//		return true;
+//	}
+//private:
+//	Node* _root = nullptr;
+//
+//};
 
-template <class K>
-struct BSTreeNode {
-
-	K _key;
-	BSTreeNode<K>* _left;
-	BSTreeNode<K>* _right;
-	BSTreeNode(const K& val = 0) :
-		_key = val,
-		_left = nullptr,
-		_right = nullptr{}
-};
-
-
-
-template <class K>
-class BSTree {
-	typedef BSTreeNode Node;
-	BSTree() :
-		_node(nullptr){}
-
-	~BSTree() {
-		_node = nullptr;
-	}
-
-	bool Insert(const K& val) {
-		if (_root == nullptr) {
-			_root = new Node(val);
-			return true;
+namespace K {
+	template<class K>
+	struct BSTreeNode {
+		K _key;
+		BSTreeNode<K>* _left;
+		BSTreeNode<K>* _right;
+		BSTreeNode(K key)
+			:_key(key)
+			, _left(nullptr)
+			, _right(nullptr) {}
+		~BSTreeNode() {
+			//delete 
+			//cout << _key << endl;
 		}
-		Node* cur = _root;
-		Node* parent = cur;
-		while (cur) {
-			if (cur->_key > val) {
-				parent = cur;
-				cur = cur->_left;
-			}
-
-			else if (cur->_key < val) {
-				parent = cur;
-				cur = cur->_right;
+	};
+	template <class K>
+	class BSTree {
+	public:
+		typedef BSTreeNode<K> Node;
+		BSTree()
+			:_root(nullptr) {}
+		bool Insert(K key) {
+			if (_root == nullptr) {
+				_root = new Node(key);
 			}
 			else {
-				return false;
+				Node* cur = _root;
+				Node* parent = _root;
+				while (cur) {
+					if (key < cur->_key) {
+						parent = cur;
+						cur = cur->_left;
+					}
+					else if (key > cur->_key) {
+						parent = cur;
+						cur = cur->_right;
+					}
+					else {
+						return false;
+					}
+				}
+				cur = new Node(key);
+				if (key < parent->_key) {
+					parent->_left = cur;
+				}
+				else {
+					parent->_right = cur;
+				}
+			}
+			return true;
+		}
+
+		Node* Find(K key) {
+			Node* cur = _root;
+			while (cur) {
+				if (key < cur->_key) {
+					cur = cur->_left;
+				}
+				else if (key > cur->_key) {
+					cur = cur->_right;
+				}
+				else {
+					return cur;
+				}
+			}
+			return nullptr;
+		}
+
+		bool Erase(K key) {
+
+			//			5
+			//		3		     7
+			//	1		4     6		 8
+			//0	   2					9
+
+			// 叶子
+			// 只有一个孩子
+			// 有两个孩子
+			Node* parent = _root;
+			Node* cur = _root;
+			while (cur) {
+				if (key < cur->_key) {
+					parent = cur;
+					cur = cur->_left;
+				}
+				else if (key > cur->_key) {
+					parent = cur;
+					cur = cur->_right;
+				}
+				else {
+					if (cur->_left == nullptr) {
+						// 左孩子为空
+						// 1. cur为根
+						if (cur == _root) {
+							_root = cur->_right;
+						}
+						// 2. cur不为根
+						else {
+							if (parent->_left == cur) {
+								parent->_left = cur->_right;
+							}
+							else {
+								parent->_right = cur->_right;
+							}
+						}
+						delete cur;
+						cur = nullptr;
+					}
+					else if (cur->_right == nullptr) {
+						// 右孩子为空
+						// 1. cur为根
+						if (cur == _root) {
+							_root = cur->_left;
+						}
+						// 2. cur不为根
+						else {
+							if (parent->_left == cur) {
+								parent->_left = cur->_left;
+							}
+							else {
+								parent->_right = cur->_left;
+							}
+						}
+						delete cur;
+						cur = nullptr;
+					}
+					else {
+						// 左右孩子都不为空
+						Node* RLNode = cur->_right;
+						Node* pRLNode = cur;
+						Node* ppRLNode = cur;
+						while (RLNode) {
+							ppRLNode = pRLNode;
+							pRLNode = RLNode;
+							RLNode = RLNode->_left;
+						}
+						cur->_key = pRLNode->_key;
+						if (ppRLNode->_left == pRLNode) {
+							ppRLNode->_left = pRLNode->_right;
+						}
+						else {
+							ppRLNode->_right = pRLNode->_right;
+						}
+						delete pRLNode;
+						pRLNode = nullptr;
+					}
+					return true;
+				}
+			}
+			return false;
+		}
+		void InOrder() {
+			_InOrder(_root);
+			cout << endl;
+		}
+		void _InOrder(Node* root) {
+			if (root != nullptr) {
+				_InOrder(root->_left);
+				cout << root->_key << " ";
+				_InOrder(root->_right);
 			}
 		}
-		
-		if (parent->_left == cur) {
-			parent->_left = new Node(val);
-		}
-		else {
-			parent->_right = new Node(val);
-		}
-		return true;
-	}
-private:
-	Node* _root = nullptr;
+	private:
+		Node* _root;
+	};
+}
 
-};
+
+
+namespace KV {
+	template<class K, class V>
+	struct BSTreeNode {
+		K _key;
+		V _val;
+		BSTreeNode<K, V>* _left;
+		BSTreeNode<K, V>* _right;
+		BSTreeNode(K key, V val)
+			:_key(key)
+			, _val(val)
+			, _left(nullptr)
+			, _right(nullptr) {}
+		~BSTreeNode() {
+		}
+	};
+	template <class K, class V>
+	class BSTree {
+	public:
+		typedef BSTreeNode<K, V> Node;
+		BSTree()
+			:_root(nullptr) {}
+
+		V& operator[](K key) {
+			pair<Node*, bool> ret = Insert(key, V());
+			return ret.first->_val;
+
+		}
+		pair<Node*, bool> Insert(K key, V val) {
+			if (_root == nullptr) {
+				_root = new Node(key, val);
+				return make_pair(_root, true);
+			}
+			else {
+				Node* cur = _root;
+				Node* parent = _root;
+				while (cur) {
+					if (key < cur->_key) {
+						parent = cur;
+						cur = cur->_left;
+					}
+					else if (key> cur->_key) {
+						parent = cur;
+						cur = cur->_right;
+					}
+					else {
+						return make_pair(cur, false);
+					}
+				}
+				cur = new Node(key, val);
+				if (key < parent->_key) {
+					parent->_left = cur;
+				}
+				else {
+					parent->_right = cur;
+				}
+				return make_pair(cur, true);
+			}
+		}
+
+		Node* Find(K key) {
+			Node* cur = _root;
+			while (cur) {
+				if (key < cur->_key) {
+					cur = cur->_left;
+				}
+				else if (key > cur->_key) {
+					cur = cur->_right;
+				}
+				else {
+					return cur;
+				}
+			}
+			return nullptr;
+		}
+
+		bool Erase(K key) {
+
+			//			5
+			//		3		     7
+			//	1		4     6		 8
+			//0	   2					9
+
+			// 叶子
+			// 只有一个孩子
+			// 有两个孩子
+			Node* parent = _root;
+			Node* cur = _root;
+			while (cur) {
+				if (key < cur->_key) {
+					parent = cur;
+					cur = cur->_left;
+				}
+				else if (key > cur->_key) {
+					parent = cur;
+					cur = cur->_right;
+				}
+				else {
+					if (cur->_left == nullptr) {
+						// 左孩子为空
+						// 1. cur为根
+						if (cur == _root) {
+							_root = cur->_right;
+						}
+						// 2. cur不为根
+						else {
+							if (parent->_left == cur) {
+								parent->_left = cur->_right;
+							}
+							else {
+								parent->_right = cur->_right;
+							}
+						}
+						delete cur;
+						cur = nullptr;
+					}
+					else if (cur->_right == nullptr) {
+						// 右孩子为空
+						// 1. cur为根
+						if (cur == _root) {
+							_root = cur->_left;
+						}
+						// 2. cur不为根
+						else {
+							if (parent->_left == cur) {
+								parent->_left = cur->_left;
+							}
+							else {
+								parent->_right = cur->_left;
+							}
+						}
+						delete cur;
+						cur = nullptr;
+					}
+					else {
+						// 左右孩子都不为空
+						Node* RLNode = cur->_right;
+						Node* pRLNode = cur;
+						Node* ppRLNode = cur;
+						while (RLNode) {
+							ppRLNode = pRLNode;
+							pRLNode = RLNode;
+							RLNode = RLNode->_left;
+						}
+						cur->_key = pRLNode->_key;
+						cur->_val = pRLNode->_val;
+						if (ppRLNode->_left == pRLNode) {
+							ppRLNode->_left = pRLNode->_right;
+						}
+						else {
+							ppRLNode->_right = pRLNode->_right;
+						}
+						delete pRLNode;
+						pRLNode = nullptr;
+					}
+					return true;
+				}
+			}
+			return false;
+		}
+		void InOrder() {
+			_InOrder(_root);
+			cout << endl;
+		}
+		void _InOrder(Node* root) {
+			if (root != nullptr) {
+				_InOrder(root->_left);
+				cout << root->_key << ":" << root->_val << " ";
+				_InOrder(root->_right);
+			}
+		}
+	private:
+		Node* _root;
+	};
+}
--- Class_of_CPP/2022_1_24_BinarySearchTree/test.cpp
+++ Class_of_CPP/2022_1_24_BinarySearchTree/test.cpp
@@ -1,40 +1,113 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "BSTree.h"
+void Test1() {
+	//BSTree<int> root;
+	//root.Push(1);
+	//cout << (2 ^ 3) << endl;
+	//cout << (6 ^ 7) << endl;
+	K::BSTree<int> tree;
+	tree.Insert(1);
+	tree.Insert(3);
+	tree.Insert(5);
+	tree.Insert(7);
 
+	tree.Insert(-7);
+	tree.Insert(7);
+	tree.Insert(999);
+	tree.Insert(9);
+	tree.Insert(12);
+	tree.InOrder();
+	tree.Erase(1);
+	tree.InOrder();
+
+
+	/*tree.Erase(5);
+	tree.InOrder();
+
+	tree.Erase(999);
+	tree.InOrder();
+
+	tree.Erase(1);
+	tree.InOrder();
+
+	tree.Erase(3);
+	tree.InOrder();
+
+	tree.Erase(7);
+	tree.InOrder();
+
+	tree.Erase(-7);
+	tree.InOrder();
+
+	tree.Erase(9);
+	tree.InOrder();
+
+
+
+	tree.Erase(12);
+	tree.InOrder();*/
+
+
+	//cout << (tree.Find(999))->_val << endl;
+}
+void Test2() {
+	KV::BSTree<string, int> tree;
+	vector<string> arr = { "苹果", "香蕉", "苹果", "西瓜" , "梨" };
+	for (auto e : arr) {
+		KV::BSTreeNode<string, int>* ret = tree.Find(e);
+		if (ret == nullptr) {
+			tree.Insert(e, 1);
+		}
+		else {
+			ret->_val++;
+		}
+	}
+	tree.InOrder();
+}
+
+void Test3() {
+	KV::BSTree<string, int> tree;
+	vector<string> arr = { "苹果", "香蕉", "苹果", "西瓜" , "梨" };
+	for (auto e : arr) {
+		tree[e]++;
+	}
+	tree.InOrder();
+}
 int main() {
-	BSTree<int> root;
-	root.Push(1);
+	//Test1();
+	Test2();
+	Test3();
 	return 0;
 }
 
 
-else if(cur->left!=nullptr && cur->right!=nullptr){
-	if (parent->left == cur) {
-		BTNode* tmpRoot = cur;
-		while (cur->right != nullptr) {
-			BTNode* tmpparent = cur;
-			cur = cur->right;
-			if (cur->right != nullptr) {
-				parent = cur;
-			}
-		}
-		//找到了最右的节点cur
-		tmpRoot->_key = parent->right->_key;
-		delete parent->right;
-		parent->right = nullptr;
-	}
-	else if(parent->right == cur){
-		BTNode* tmpRoot = cur;
-		while (cur->left != nullptr) {
-			BTNode* tmpparent = cur;
-			cur = cur->left;
-			if (cur->left != nullptr) {
-				parent = cur;
-			}
-		}
-		tmpRoot->_key = parent->left->_key;
-		delete parent->left;
-		parent->left == nullptr;
-		
-	}
-}
+//else if(cur->left!=nullptr && cur->right!=nullptr){
+//	if (parent->left == cur) {
+//		BTNode* tmpRoot = cur;
+//		while (cur->right != nullptr) {
+//			BTNode* tmpparent = cur;
+//			cur = cur->right;
+//			if (cur->right != nullptr) {
+//				parent = cur;
+//			}
+//		}
+//		//找到了最右的节点cur
+//		tmpRoot->_key = parent->right->_key;
+//		delete parent->right;
+//		parent->right = nullptr;
+//	}
+//	else if(parent->right == cur){
+//		BTNode* tmpRoot = cur;
+//		while (cur->left != nullptr) {
+//			BTNode* tmpparent = cur;
+//			cur = cur->left;
+//			if (cur->left != nullptr) {
+//				parent = cur;
+//			}
+//		}
+//		tmpRoot->_key = parent->left->_key;
+//		delete parent->left;
+//		parent->left == nullptr;
+//		
+//	}
+//}
